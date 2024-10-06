7af898627b6c6ba7f4ff8ba1abf9c708015d0bc0
sky4564 <qw486512@gmail.com>
2024-10-07 01:03:39 +0900

    add : import and export components

---
+++ src/components/Logo.jsx
@@ -0,0 +1,18 @@
+import reactLogo from '@/assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export default function Logo() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  )
+}
---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,12 @@
+import Logo from "./Logo"
+import Counter from "./Counter"
+
+
+export default function WelcomePage() {
+  return (
+    <>
+      <Logo></Logo>
+      <Counter></Counter>
+    </>
+  )
+}
