10c4c8304a3e543cb7ef9b8d471c1cbb632fd11a
sky4564 <qw486512@gmail.com>
2024-10-08 01:14:56 +0900

    pratice useState

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -10,17 +10,17 @@ export default function CounterPage() {
     }
 
     function liftUp(data) {
-        setLiftItem((x) => x + data)
+        setLiftItem(data)
     }
 
     return (
         <div className="min-h-screen bg-slate-600 p-4">
+            <div className="mt-4 bg-white shadow-md rounded-lg p-6">
+                <h3 className="text-lg font-semibold mb-2 text-gray-700">Lifted Item</h3>
+                <p className="text-3xl font-bold text-gray-800">{liftItem}</p>
+            </div>
             <div className="max-w-md mx-auto">
                 <Counter cnt={cnt} cntUp={cntUp} liftUp={liftUp} />
-                <div className="mt-4 bg-white shadow-md rounded-lg p-6">
-                    <h3 className="text-lg font-semibold mb-2 text-gray-700">Lifted Item</h3>
-                    <p className="text-3xl font-bold text-gray-800">{liftItem}</p>
-                </div>
             </div>
         </div>
     )
