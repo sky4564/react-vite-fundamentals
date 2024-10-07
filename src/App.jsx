a958a8640ca7cd23c4fb5e713cec4380cc56a3e2
sky4564 <qw486512@gmail.com>
2024-10-07 14:21:05 +0900

    ADD: styling compoents tailwindcss default settings

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <>
       <WelcomePage></WelcomePage>
+      <div className="p-4 bg-blue-500 text-white">
+        <h1 className="text-2xl font-bold">Hello, Tailwind CSS with Vite!</h1>
+      </div>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
