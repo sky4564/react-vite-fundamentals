aca81cb8cffb158e91479893fd7b87af722b527e
sky4564 <qw486512@gmail.com>
2024-10-08 00:34:22 +0900

    FEAT : add lifting state up (child -> parent

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,36 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 
-export default function Counter() {
+Counter.propTypes = {
+  cnt: PropTypes.number.isRequired,
+  cntUp: PropTypes.func.isRequired,
+  liftUp: PropTypes.func.isRequired
+}
+
+export default function Counter({ cnt, cntUp, liftUp }) {
   const [count, setCount] = useState(0)
   return (
     <div className="flex flex-col items-center text-white p-8">
-      <div className="p-6 bg-white bg-opacity-10 rounded-lg shadow-lg backdrop-filter backdrop-blur-lg">
-
+      <div className="p-6 flex flex-col bg-white bg-opacity-10 rounded-lg shadow-lg backdrop-filter backdrop-blur-lg">
         <button
           className="px-8 py-3 mb-6 text-lg font-semibold text-white bg-gradient-to-r from-purple-500 to-cyan-500 rounded-full hover:from-purple-600 hover:to-cyan-600 transition-all duration-300 ease-in-out transform hover:scale-105"
           onClick={() => setCount((count) => count + 1)}
         >
           Count is {count}
+        </button>
+
+        <button
+          className="px-8 py-3 mb-6 text-lg font-semibold text-white bg-gradient-to-r from-purple-500 to-cyan-500 rounded-full hover:from-purple-600 hover:to-cyan-600 transition-all duration-300 ease-in-out transform hover:scale-105"
+          onClick={() => liftUp(count)}
+        >
+          Data lifting state up ! ⬆️⬆️⬆️
+        </button>
+
+        <button
+          className="px-8 py-3 mb-6 text-lg font-semibold text-white bg-gradient-to-r from-purple-500 to-cyan-500 rounded-full hover:from-purple-600 hover:to-cyan-600 transition-all duration-300 ease-in-out transform hover:scale-105"
+          onClick={() => cntUp()}
+        >
+          Props Count is {cnt}
         </button>
 
         <p className="text-gray-300">
